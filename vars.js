22b91eb7bea3d525d92c0a03b9bcfd59913de865
AndyBullet <55254846+AndyBullet@users.noreply.github.com>
2021-11-22 19:02:37 +0100

    Create vars.js

---
+++ vars.js
@@ -0,0 +1,3 @@
+var pulsante = document.querySelectorAll("input[value=Generate]")[0];
+var va0 = document.querySelectorAll("input[type=number]")[0].value;
+var va1 = document.querySelectorAll("input[type=number]")[1].value;
