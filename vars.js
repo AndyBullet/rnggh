fd59e4c46f2b8cb47725859856603c952e5b4c88
AndyBullet <55254846+AndyBullet@users.noreply.github.com>
2021-11-22 22:53:32 +0100

    Update vars.js

--- vars.js
+++ vars.js
@@ -1,4 +1,4 @@
-var pulsante = document.querySelectorAll("input[type=button]")[0];
-var va0 = document.querySelectorAll("input[type=number]")[0].value;
-var va1 = document.querySelectorAll("input[type=number]")[1].value;
+var pulsante = document.querySelectorAll("input[type='button']")[0];
+var va0 = document.querySelectorAll("input[type='number']")[0].value;
+var va1 = document.querySelectorAll("input[type='number']")[1].value;
 var generated = document.getElementsByTagName('span')[5];
