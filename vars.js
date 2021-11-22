10380e5f379af9d6db92e151e8d16b4c8c32975f
AndyBullet <55254846+AndyBullet@users.noreply.github.com>
2021-11-22 19:24:06 +0100

    Update vars.js

--- vars.js
+++ vars.js
@@ -1,3 +1,22 @@
-var pulsante = document.querySelectorAll("input[value=Generate]")[0];
-var va0 = document.querySelectorAll("input[type=number]")[0].value;
-var va1 = document.querySelectorAll("input[type=number]")[1].value;
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+var pulsante;
+var va0;
+var va1;
+
+if(inIframe = false){
+  pulsante = document.querySelectorAll("input[value=Generate]")[0];
+  va0 = document.querySelectorAll("input[type=number]")[0].value;
+  va1 = document.querySelectorAll("input[type=number]")[1].value;
+}
+else if(inIframe = true){
+  pulsante = document.getElementsByTagName('iframe')[0].contentWindow.document.querySelectorAll("input[type='number']")[0].value;
+  va0 = document.getElementsByTagName('iframe')[0].contentWindow.document.querySelectorAll("input[type=number]")[0].value;
+  va1 = document.getElementsByTagName('iframe')[0].contentWindow.document.querySelectorAll("input[type=number]")[1].value;
+}
