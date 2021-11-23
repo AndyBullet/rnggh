fc50a2263599605c4b6471dc41655d22f33a75f4
AndyBullet <55254846+AndyBullet@users.noreply.github.com>
2021-11-23 01:02:38 +0100

    Update and rename variables.js to varss.js

--- varss.js
+++ varss.js
@@ -3,6 +3,6 @@ var va1;
 var pulsante = document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='button']")[0];
 var generated = document.getElementsByTagName("iframe")[0].contentWindow.document.getElementsByTagName('span')[5];
 setInterval(()=>{
-  document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='number']")[0].value;
-  document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='number']")[1].value;
+  va0 = document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='number']")[0].value;
+  va1 = document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='number']")[1].value;
 }, 500);
