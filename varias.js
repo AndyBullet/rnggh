0f804793b9b6944e8b480f6f6609b36b9122d417
AndyBullet <55254846+AndyBullet@users.noreply.github.com>
2021-11-23 01:00:20 +0100

    Update and rename varias.js to vars.js

--- varias.js
+++
@@ -1,4 +0,0 @@
-var pulsante = document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='button']")[0];
-var va0 = document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='number']")[0].value;
-var va1 = document.getElementsByTagName("iframe")[0].contentWindow.document.querySelectorAll("input[type='number']")[1].value;
-var generated = document.getElementsByTagName("iframe")[0].contentWindow.document.getElementsByTagName('span')[5];
